18fad91da7f35748a6141292076d8b7ad705ad58
Martin Fernandez <martinnfernandezz.20@gmail.com>
2024-01-26 12:53:54 -0300

    Funciona el Form

---
+++ js/form.js
@@ -0,0 +1,34 @@
+document.getElementById('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const btn = document.getElementById('button');
+    btn.value = 'Enviando formulario...';
+
+    const serviceID = 'default_service';
+    const templateID = 'template_r33azd8';
+
+    emailjs.sendForm(serviceID, templateID, this)
+        .then(() => {
+            btn.value = 'Enviar correo';
+            Toastify({
+                text: "¡Correo enviado con éxito!",
+                duration: 4000,
+                close: true,
+                gravity: "top", // or "top"
+                position: "center", // or "left"
+                backgroundColor: "#eece1a"
+            }).showToast();
+            this.reset(); // Esto reinicia el formulario
+        }, (err) => {
+            btn.value = 'Enviar correo';
+            Toastify({
+                text: "Error al enviar el correo. Por favor, inténtalo de nuevo.",
+                duration: 3000,
+                close: true,
+                gravity: "top", // or "top"
+                position: "center", // or "left"
+                backgroundColor: "red"
+            }).showToast();
+            console.error(JSON.stringify(err));
+        });
+});
